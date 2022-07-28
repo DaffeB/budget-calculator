f2c0dca1d1c4969062058ebeee7b16f883b1afe1
Dafina Beqiraj <93763212+DaffeB@users.noreply.github.com>
2022-07-28 11:37:29 +0200

    Creating Array of Expenses

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import React from 'react';
 import './App.css';
 import Alert from './Components/Alert';
 import ExpenseForm from './Components/ExpenseForm';
-import ExpenseItem from './Components/ExpenseItem';
 import ExpenseList from './Components/ExpenseList';
+
+
+
+const initialExpenses = [
+  {id: 1, charge:"rent", amount: 1600},
+  {id: 2, charge:"car payment", amount: 400},
+  {id: 3, charge:"credit card bill", amount: 1200}
+]
+
+console.log(initialExpenses);
 
 function App() {
   return (
     <div className="App">
       <h1>hello from app</h1>
       <ExpenseForm></ExpenseForm>
-      <ExpenseItem></ExpenseItem>
       <ExpenseList></ExpenseList>
       <Alert></Alert>
 
