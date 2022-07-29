83239b17f0314e49663b212857a2e13dbefbb1b6
Dafina Beqiraj <93763212+DaffeB@users.noreply.github.com>
2022-07-29 09:21:00 +0200

    Expensive List

--- src/Components/ExpenseList.js
+++ src/Components/ExpenseList.js
@@ -1,19 +1,28 @@
 import React from 'react'
 import ExpenseItem from './ExpenseItem';
 import Item from './ExpenseItem';
+import { MdDelete } from 'react-icons/md'
 
 
 
 
-const ExpenseList = ({ expenses }) =>{
-    return(
+
+
+const ExpenseList = ({ expenses }) => {
+    return (
         <div>
+
             <ul className='list'>
                 {expenses.map(expense => {
-                    return <Item key={expense.id}  expense= {expense} />
+                    return <Item key={expense.id} expense={expense} />
                 })}
             </ul>
-            {expenses.length > 0 && <button className='btn'>Clear Expenses</button>}
+            {expenses.length > 0 && (
+                <button className='btn'>Clear Expenses
+                    <MdDelete className='btn-icon'></MdDelete>
+
+                </button>
+            )}
         </div>
     )
 }
